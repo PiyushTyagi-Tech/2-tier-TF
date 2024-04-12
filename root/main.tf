53927f94315518347dc582ae530b98b231b15922
Piyush-Tech <Piyushtyagi8085@gmail.com>
2024-04-12 00:25:48 -0400

    Creating Security group

--- root/main.tf
+++ root/main.tf
@@ -3,13 +3,20 @@ provider "aws" {
 }
 
 module "vpc" {
-    source = "../modules/vpc"
-    region = var.region
-    project_name = var.project_name
-    cidr = var.cidr
-    pri_sub_2a = var.pri_sub_2a
-    pri_sub_2b = var.pri_sub_2b
-    pub_sub_2a = var.pub_sub_2a
-    pub_sub_2b = var.pub_sub_2b
-  
+  source       = "../modules/vpc"
+  region       = var.region
+  project_name = var.project_name
+  cidr         = var.cidr
+  pri_sub_2a   = var.pri_sub_2a
+  pri_sub_2b   = var.pri_sub_2b
+  pub_sub_2a   = var.pub_sub_2a
+  pub_sub_2b   = var.pub_sub_2b
+
+}
+
+#creating security group
+module "sg" {
+  source = "../modules/securitygroup"
+  vpc_id = module.vpc.vpc_id
+  my_ip  = var.my_ip
 }
--- root/variables.tf
+++ root/variables.tf
@@ -1,25 +1,27 @@
 variable "region" {
     description = "This our aws region variable"
-    default = "us-east-2"
+    #default = "us-east-2"
       
 }
 variable "project_name" {
     description = "This is the project name variable"
-    default = "two-tier-app"
+    #default = "two-tier-app"
   
 }
 variable "cidr" {
-    default = "10.0.0.0/16"
+    #default = "10.0.0.0/16"
 }
 variable "pub_sub_2a" {
-    default = "10.0.0.0/20"
+    #default = "10.0.0.0/20"
 }
 variable "pri_sub_2a" {
-    default = "10.0.16.0/20"
+    #default = "10.0.16.0/20"
 }
 variable "pub_sub_2b" {
-    default = "10.0.128.0/20"
+    #default = "10.0.128.0/20"
 }
 variable "pri_sub_2b" {
-    default = "10.0.144.0/20"
+    #default = "10.0.144.0/20"
 }
+
+variable "my_ip" {}
